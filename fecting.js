f63f662f90185f8d585bb062a8dba5556b0568b2
mdrahmatullahamn <rmd831219@gmail.com>
2023-03-09 16:53:40 +0530

    update

---
+++ fecting.js
@@ -0,0 +1,18 @@
+function dataFetching(){
+    alert("working")
+    fetch("https://fakestoreapi.com/products")
+    .then(res => res.json())
+    .then(json => gettingproducts(json))
+
+}
+function gettingproducts(){
+    console.log(products)
+    var diFromHtml = document.getElementById('products');
+
+    var allProducts = [];
+    for( var i = 0; i<products.length;i++){
+        allProducts =+  `<div>  <img src='${products[i].image}'/><h1>${products[i].title}</h1></div>`
+    }
+
+        diFromHtml.innerHTML=  allProducts;
+}
